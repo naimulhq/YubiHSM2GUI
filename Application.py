1863fbcd831abfffc765600fafefcef9be0aa5f5
naimulhq <naimul@ucsb.edu>
2021-07-19 16:42:52 -0700

    Naimul - Checking login through exceptions

--- Application.py
+++ Application.py
@@ -4,7 +4,10 @@ import tkinter as tk
 import PIL
 from PIL import ImageTk
 from PIL import Image
+import struct
 from tkinter import messagebox
+from yubihsm import YubiHsm
+from yubihsm.exceptions import YubiHsmAuthenticationError
 
 
 class Application:
@@ -12,6 +15,8 @@ class Application:
     def __init__(self):
         self.GUI_GEOMETRY = "700x300"
         self.IMAGE_PATH = "/home/naimul/NaimulRepo/YubiHSM2Tool/LC.png"
+        self.HSM_URL = "http://127.0.0.1:12345"
+        self.hsm = YubiHsm.connect(self.HSM_URL)
         self.createLoginPage()
         pass
 
@@ -22,15 +27,11 @@ class Application:
         self.loginPage.geometry(self.GUI_GEOMETRY)
         img = ImageTk.PhotoImage(Image.open(self.IMAGE_PATH))
         panel = tk.Label(self.loginPage,image=img)
-        self.usernameEntry = tk.Entry(self.loginPage)
         self.objectIDEntry = tk.Entry(self.loginPage)
         self.passwordEntry = tk.Entry(self.loginPage,show='*')
-        self.usernameInstructions = tk.Label(self.loginPage, text="Enter Username")
         self.objectIDInstructions = tk.Label(self.loginPage, text="Enter Object ID")
         self.passwordInstructions = tk.Label(self.loginPage, text="Enter Password")
         self.submitButtonForLogin = tk.Button(self.loginPage, text="Submit", command=self.checkLoginCredentials)
-        self.usernameInstructions.pack()
-        self.usernameEntry.pack()
         self.objectIDInstructions.pack()
         self.objectIDEntry.pack()
         self.passwordInstructions.pack()
@@ -40,10 +41,15 @@ class Application:
         self.loginPage.mainloop()
 
     def checkLoginCredentials(self):
-        pass
+        try:
+            self.session = self.hsm.create_session_derived(int(self.objectIDEntry.get()), self.passwordEntry.get())
+        except ValueError:
+            messagebox.showerror(title='Invalid ObjectID', message='Object ID has to be an integer')
+        except YubiHsmConnectionError:
+            messagebox.showerror(title='Connection Error', message='Can not communicate with YubiHSM. Check connection.')
+        except YubiHsmAuthenticationError:
+            messagebox.showerror(title='Invalid Credentials', message='The username or password is incorrect')
 
-
-    
 
 if __name__ == '__main__':
     app = Application()
