e427256f65d59237b0f4d60143f8740b29def9af
naimulhq <naimul@ucsb.edu>
2021-07-19 15:55:46 -0700

    Naimul - Added branding, change widget locations

--- Application.py
+++ Application.py
@@ -10,7 +10,8 @@ from tkinter import messagebox
 class Application:
     
     def __init__(self):
-        self.GUI_GEOMETRY = "700x200"
+        self.GUI_GEOMETRY = "700x300"
+        self.IMAGE_PATH = "/home/naimul/NaimulRepo/YubiHSM2Tool/LC.png"
         self.createLoginPage()
         pass
 
@@ -19,6 +20,8 @@ class Application:
         self.loginPage.title('YubiHSM2 Tool')
         self.loginPage.resizable(0,0)
         self.loginPage.geometry(self.GUI_GEOMETRY)
+        img = ImageTk.PhotoImage(Image.open(self.IMAGE_PATH))
+        panel = tk.Label(self.loginPage,image=img)
         self.usernameEntry = tk.Entry(self.loginPage)
         self.objectIDEntry = tk.Entry(self.loginPage)
         self.passwordEntry = tk.Entry(self.loginPage,show='*')
@@ -32,7 +35,8 @@ class Application:
         self.objectIDEntry.pack()
         self.passwordInstructions.pack()
         self.passwordEntry.pack()
-        self.submitButtonForLogin.pack(side='right')
+        panel.place(x=70,y=200)
+        self.submitButtonForLogin.place(x=600,y=100)
         self.loginPage.mainloop()
 
     def checkLoginCredentials(self):
