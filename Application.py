a38d12fb8a377d1f6699017f7b543e28bd5bc527
naimulhq <naimul@ucsb.edu>
2021-07-19 15:43:40 -0700

    Naimul - Added Button, Password entries are hidden

--- Application.py
+++ Application.py
@@ -10,25 +10,33 @@ from tkinter import messagebox
 class Application:
     
     def __init__(self):
+        self.GUI_GEOMETRY = "700x200"
         self.createLoginPage()
         pass
 
     def createLoginPage(self):
         self.loginPage = tk.Tk()
         self.loginPage.title('YubiHSM2 Tool')
+        self.loginPage.resizable(0,0)
+        self.loginPage.geometry(self.GUI_GEOMETRY)
         self.usernameEntry = tk.Entry(self.loginPage)
         self.objectIDEntry = tk.Entry(self.loginPage)
-        self.passwordEntry = tk.Entry(self.loginPage)
+        self.passwordEntry = tk.Entry(self.loginPage,show='*')
         self.usernameInstructions = tk.Label(self.loginPage, text="Enter Username")
         self.objectIDInstructions = tk.Label(self.loginPage, text="Enter Object ID")
         self.passwordInstructions = tk.Label(self.loginPage, text="Enter Password")
+        self.submitButtonForLogin = tk.Button(self.loginPage, text="Submit", command=self.checkLoginCredentials)
         self.usernameInstructions.pack()
         self.usernameEntry.pack()
         self.objectIDInstructions.pack()
         self.objectIDEntry.pack()
         self.passwordInstructions.pack()
         self.passwordEntry.pack()
+        self.submitButtonForLogin.pack(side='right')
         self.loginPage.mainloop()
+
+    def checkLoginCredentials(self):
+        pass
 
 
     
